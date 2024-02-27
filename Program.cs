5cbb45889934f9fcc50c68dc4549605cf445652e
rafliad <radenistri@gmail.com>
2024-02-27 15:05:03 +0700

    change

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Masukkan nama:");
+string namaMahasiswa = Console.ReadLine();
+Console.WriteLine("Hello, " + namaMahasiswa + "!");
